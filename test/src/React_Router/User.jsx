c2c8fa38338591ddb1b6e8c3488ecbfe7c974f77
Aniket Shelake <aniketshelake151@sitcoe.org.in>
2024-02-07 19:00:31 +0530

    created user page for useParams hook

---
+++ test/src/React_Router/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+
+  const {userId} = useParams();  
+  return (
+    <div>
+        <h1>User Information Page</h1>
+      <h2>User : {userId}</h2>
+    </div>
+  )
+}
+
+export default User
