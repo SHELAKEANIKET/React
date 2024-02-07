0d75bce168e5ed373cf736713354f26414359c56
Aniket Shelake <aniketshelake151@sitcoe.org.in>
2024-02-07 22:30:58 +0530

    password generator

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import PasswordGenerator from "./PasswordGenerator";
+
+function App() {
+  return (
+    <div className="w-full h-screen mx-auto flex justify-center items-center flex-col bg-slate-800 text-white">
+      <h1 className="text-white text-center my-4 text-2xl font-bold ">Password Generator 🔒</h1>
+      <PasswordGenerator />
+    </div>
+  );
+}
+
+export default App;
